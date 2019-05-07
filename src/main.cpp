9763364da729a927a0ad6d8067ac1ef9f2f2ab54
Henrike100 <henrikem100@gmail.com>
2019-05-07 11:43:11 -0300

    código inicial de abrir fila

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <iostream>
 
-int main() {
-    printf("Hello Chat!\n");
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    char queue_name[20];
+    strcpy(queue_name, "/chat-");
+    strcat(queue_name, argv[1]);
+    mqd_t queue;
+    struct mq_attr attr;
+    char msg[501];
+
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(msg);
+    attr.mq_flags = 0;
+
+    // Abre fila
+    if ((queue = mq_open (queue_name, O_RDWR|O_CREAT, 0666, &attr)) < 0) {
+        perror ("mq_open");
+        exit (1);
+    }
 
     return 0;
 }
